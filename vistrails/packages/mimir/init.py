895f7204fbf56a9cdf3f46afae72969c2fc64a24
Michael Brachmann <mrb24@buffalo.edu>
2018-08-13 13:51:43 -0400

    minor change in mimir package
    Don't shutdown mimir on finalize

--- vistrails/packages/mimir/init.py
+++ vistrails/packages/mimir/init.py
@@ -171,8 +171,8 @@ def initialize():
 
 def finalize():
     print("Shutting Down Gateway...")
-    global _mimir
-    _mimir.shutdown()
+    #global _mimir
+    #_mimir.shutdown()
     global _gateway
     _gateway.shutdown()
 
